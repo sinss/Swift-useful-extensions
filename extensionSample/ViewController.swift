a1aeeccea55c0c2ba3aee4d68a4bd915bc73082f
Leo Chang <sinss0000@gmail.com>
2014-07-08 14:59:55 +0800

    Initial Commit

---
+++ extensionSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  extensionSample
+//
+//  Created by Leo Chang on 7/8/14.
+//  Copyright (c) 2014 Perfectidea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ extensionSampleTests/extensionSampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  extensionSampleTests.swift
+//  extensionSampleTests
+//
+//  Created by Leo Chang on 7/8/14.
+//  Copyright (c) 2014 Perfectidea. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class extensionSampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
